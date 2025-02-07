3179e0948f8cb98792f3bf65834c06c50f36194b
Angel Mendoza <angel.mendoza@mec.science>
2025-02-07 21:33:42 +0000

    updated lab 2_1

---
+++ Week02/Lab/mygrade.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number = 90;
+     char lettergrade = 'A';
+
+    cout << "I expect myself to earn an " << number << " Which is a letter grade of " << lettergrade << endl;
+    return 0;
+}
---
+++ Week02/Lab/temperature.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+const double NORMAL_TEMP = 98.6;	        // normal body temperature in Fahrenheit
+
+int main()
+{
+	double celsius;		                    // definition of celsius temperature
+	celsius == (NORMAL_TEMP - 32) * 5 / 9;	// computes celsius temperature: 37 degrees
+        
+	// Fill in the code for the cout statement that will output (with description)
+	// the celsius temperature. 
+
+	return 0;
+}
