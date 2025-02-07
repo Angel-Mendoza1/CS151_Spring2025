8dc41f0a51bad03fc4458d5ba3844dd7caf65462
Angel Mendoza <angel.mendoza@mec.science>
2025-02-07 21:44:25 +0000

    Repository Update

--- Week02/Lab/temperature.cpp
+++ Week02/Lab/temperature.cpp
@@ -6,8 +6,8 @@ const double NORMAL_TEMP = 98.6;	        // normal body temperature in Fahrenhei
 int main()
 {
 	double celsius;		                    // definition of celsius temperature
-	celsius == (NORMAL_TEMP - 32) * 5 / 9;	// computes celsius temperature: 37 degrees
-        
+	celsius == ( - 32) * 5 / 9;	// computes celsius temperature: 37 degrees
+        cout << "The normal body temperature in celsius is " << celsius << endl;
 	// Fill in the code for the cout statement that will output (with description)
 	// the celsius temperature. 
 
