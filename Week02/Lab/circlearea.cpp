57028450024aadccce88240dc384c9e2839c1591
Angel Mendoza <angel.mendoza@mec.science>
2025-02-07 22:13:11 +0000

    <commit-message>

---
+++ Week02/Lab/circlearea.cpp
@@ -0,0 +1,28 @@
+// This program will output the circumference and area
+// of the circle with a given radius.
+
+#include <iostream>
+using namespace std;
+
+const double PI = 3.14;
+const double RADIUS = 5.4;
+
+int main()
+{
+	double area;								// definition of area of circle 
+    double circumference;
+	circumference = 2 * PI * RADIUS;	// computes circumference
+				// definition of circumference 
+	area = PI * RADIUS * RADIUS;		// computes area
+    
+
+    cout <<"The area of the circle with a radius of 5.4 is " << area <<endl;
+
+	// Fill in the code for the cout statement that will output (with description)
+	// the circumference
+
+	// Fill in the code for the cout statement that will output (with description)
+	// the area of the circle
+
+	return 0;
+}
--- Week02/Lab/temperature.cpp
+++ Week02/Lab/temperature.cpp
@@ -6,7 +6,7 @@ const double NORMAL_TEMP = 98.6;	        // normal body temperature in Fahrenhei
 int main()
 {
 	double celsius;		                    // definition of celsius temperature
-	celsius == ( - 32) * 5 / 9;	// computes celsius temperature: 37 degrees
+	celsius = (NORMAL_TEMP - 32) * 5 / 9;	// computes celsius temperature: 37 degrees
         cout << "The normal body temperature in celsius is " << celsius << endl;
 	// Fill in the code for the cout statement that will output (with description)
 	// the celsius temperature. 
